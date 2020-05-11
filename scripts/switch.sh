cc6a86db4ed8cf82480d61c72fc5ae8515d0c9c3
minkiLee <37613051+midaslmg94@users.noreply.github.com>
2020-05-11 16:04:36 +0900

    오타 수정

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -7,7 +7,7 @@ function switch_proxy(){
   IDLE_PORT=$(find_idle_port)
   echo "> 전환할 포트: $IDLE_PORT"
   echo "> 포트 전환"
-  echo "> set \$service_url http:127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service_url.inc
+  echo "> set \$service_url http:127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
   echo "> Nginx Reload"
   sudo service nginx reload 
 }
