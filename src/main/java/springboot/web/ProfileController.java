e12fa7a3863891da197a40c5f942abd286730c3d
minkiLee <37613051+midaslmg94@users.noreply.github.com>
2020-05-11 12:58:40 +0900

    application-real1,2 수정

--- src/main/java/springboot/web/ProfileController.java
+++ src/main/java/springboot/web/ProfileController.java
@@ -19,7 +19,7 @@ public class ProfileController {
 		List<String>profiles = Arrays.asList(env.getActiveProfiles()); // 현재 실행중인 ActiveProfile을 모두 가져옴
 		String defaultProfile = profiles.isEmpty()?"default":"profiles.get(0)";
 		List<String>realProfiles = Arrays.asList("real", "real1", "real2");
-		return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);	
+		return profiles.stream().filter(realProfiles::contains).findAny().orElse(defaultProfile);
 		
 	}
 	
